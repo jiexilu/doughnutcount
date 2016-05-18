703d23a41dbca52fcccbf87150d42891aab9a084
Cici Lu <jiexilu@umich.edu>
2016-05-17 22:11:57 -0700

    added emojis and, instead of alert, it now changes the html

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ function Convert() {
 	var calories_today = document.getElementById("calories_today").value;
 	var calories_in_doughnut = 195;
 
-	var num_doughnuts = calories_today/calories_in_doughnut;
-	alert("You burned " + num_doughnuts + " doughnuts today!");
+	var num_doughnuts = Math.round(calories_today/calories_in_doughnut * 100) / 100;
+	document.getElementById("doughnutCount").innerHTML = "You burned " + num_doughnuts + " doughnuts today!<br>";
+	
+	for (var i = 0; i < Math.round(num_doughnuts); i++) {
+		document.getElementById("doughnutCount").innerHTML += ":doughnut: ";
+	}
+	emojify.run(document.getElementById("doughnutCount"));
 }
